e59ba319f19069cc65c352d8fde14564fe65779e
arshdeep <arshdeep.m@greyorange.sg>
2018-12-20 18:19:02 +0530

    added fetch api appended state with new users TODO: delete duplicate users

--- src/PearsonUsers.js
+++ src/PearsonUsers.js
@@ -31,11 +31,42 @@ export class PearsonUsers extends Component {
     };
   }
 
+  componentDidMount() {
+    this._fetchUsers();
+  }
+  _fetchUsers = async (page = 1, per_page = 10) => {
+    let jsonObj = await fetch(
+      `https://reqres.in/api/users?page=${page}&per_page=${per_page}`
+    ).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network response was not ok.");
+    });
+    const users = jsonObj.data;
+    this._udpateUsersInState(users);
+  };
+
+  _udpateUsersInState = (newUsers = []) => {
+    let users = [...this.state.users, ...newUsers];
+    this.setState({ users });
+  };
+
+  _getUsersInfo = (arrayUsers = []) =>
+    arrayUsers.map(user => (
+      <div key={user.id}>
+        <div>{user.first_name}</div>
+        <div>{user.last_name}</div>
+        <div>{user.avatar}</div>
+      </div>
+    ));
+
   render() {
+    const usersInfo = this._getUsersInfo(this.state.users);
     return (
       <div className="pearon-users">
         <h1>Pearson User Management</h1>
-        {/* Render users here */}
+        {usersInfo}
       </div>
     );
   }
